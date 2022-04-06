09b66f891e696573c083f3dcd6e22f7321d04ef7
Raheel1258 <rahilahmed1258@gmail.com>
2022-04-06 16:45:40 +0500

    adds db url in env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,15 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { MongooseModule } from "@nestjs/mongoose";
 import { UserModule } from "./user/user.module";
-import { ConfigModule, ConfigService } from "@nestjs/config";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [
     UserModule,
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRootAsync({
-      useFactory: () => ({
-        uri: "mongodb+srv://noorulhuda:password12345678@print-app.0xtio.mongodb.net/PrintApp?retryWrites=true&w=majority",
+      useFactory: async () => ({
+        uri: process.env.DB_URL,
       }),
     }),
   ],
