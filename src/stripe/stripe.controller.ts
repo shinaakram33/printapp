29f709a99e1230ec4dc26f05d626c1252094a9ce
rahilahmad1258@gmail.com <rahilahmad1258@gmail.com>
2022-06-10 18:38:46 +0500

    Fixes

--- src/stripe/stripe.controller.ts
+++ src/stripe/stripe.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { ApiBasicAuth, ApiBearerAuth, ApiTags } from "@nestjs/swagger";
 import { GetUser } from "src/user/auth/get-user.decorator";
 import { User } from "src/user/user.model";
+import { AddStripeCardDto } from "./dto/add-card.dto";
 import { StripeService } from "./stripe.service";
 
 @Controller("stripe")
@@ -16,4 +17,11 @@ export class StripeController {
   async getAllCards(@GetUser() user: User): Promise<any> {
     return await this.stripeService.getAllCards(user);
   }
+
+  // @ApiBearerAuth()
+  // @UseGuards(AuthGuard("jwt"))
+  // @Post("/addCard/")
+  // async addCards(@GetUser() user: User, @Body() addCardDto: AddStripeCardDto): Promise<any> {
+  //   return this.stripeService.addCard(addCardDto, user);
+  // }
 }
